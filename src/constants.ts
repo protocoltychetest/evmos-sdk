873275d76d7348a75497f54b29c82efc59eeedaa
tychezone <protocoltyche@gmail.com>
2022-12-11 00:58:45 +0500

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,27 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | string | number
+
+export enum ChainId {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GÖRLI = 5,
+  KOVAN = 42,
+  EVMOS = 9001,
+  EVMOS_TESTNET = 9000
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const MaxUint256 = JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
